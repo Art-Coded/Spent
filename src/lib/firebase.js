b9de092b29fddaab1f7bd4067cf02d831b5f26b1
Art <lyndonehemplo1@gmail.com>
2025-04-16 00:55:30 +0800

    Updated progress in Spent webapp

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,16 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCDrFS7w7gIPo0k68WJ-aA_M99Bk9-7vfY",
-    authDomain: "spent-48159.firebaseapp.com",
-    projectId: "spent-48159",
-    storageBucket: "spent-48159.firebasestorage.app",
-    messagingSenderId: "686909666615",
-    appId: "1:686909666615:web:1152eaca93a8cc3bcc56b2",
-    measurementId: "G-7KBRKJW8SJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "spent-48159.firebaseapp.com",
+  projectId: "spent-48159",
+  storageBucket: "spent-48159.firebasestorage.app",
+  messagingSenderId: "686909666615",
+  appId: "1:686909666615:web:1152eaca93a8cc3bcc56b2",
+  measurementId: "G-7KBRKJW8SJ"
 };
 
-// ✅ Only initialize Firebase once
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export const db = getFirestore(app);
